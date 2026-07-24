f1ecb62dc6f2b227134e8e3fb6a19c3692f3919f
JeffChisambi <chisambijeffrey3@gmail.com>
2026-07-24 16:11:43 +0200

    stock err

--- utils/stock-logos.ts
+++ utils/stock-logos.ts
@@ -6,22 +6,22 @@ import { ImageSourcePropType } from "react-native";
  * instead of rendering a coloured circle with text initials.
  */
 const STOCK_LOGO_MAP: Record<string, ImageSourcePropType> = {
-  AIRTEL:   require("../assets/images/airtel.png"),
+  AIRTEL:   require("../assets/images/AIRTEL.png"),
   BHL:      require("../assets/images/BH.png"),
   FDHB:     require("../assets/images/FDH.png"),
   FMBCH:    require("../assets/images/FMBC.png"),
-  ICON:     require("../assets/images/icon.png"),
+  ICON:     require("../assets/images/ICON.png"),
   ILLOVO:   require("../assets/images/ILOVO.png"),
   MPICO:    require("../assets/images/MPICO.png"),
   NBM:      require("../assets/images/NB.png"),
-  NBS:      require("../assets/images/nbs.png"),
-  NICO:     require("../assets/images/nico.png"),
-  NITL:     require("../assets/images/nitl.png"),
+  NBS:      require("../assets/images/NBS.png"),
+  NICO:     require("../assets/images/NICO.png"),
+  NITL:     require("../assets/images/NITL.png"),
   OMU:      require("../assets/images/OMU.png"),
   PCL:      require("../assets/images/pc.png"),
   STANDARD: require("../assets/images/Standardbank.png"),
-  SUNBIRD:  require("../assets/images/sunbird.png"),
-  TNM:      require("../assets/images/tnm.png"),
+  SUNBIRD:  require("../assets/images/SUNBIRD.png"),
+  TNM:      require("../assets/images/TNM.png"),
 };
 
 /**
